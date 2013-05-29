a35015e222ea5c09ee3228ed5682b4c0eb861e63
Ben Razon <yummysteak@gmail.com>
2013-05-29 01:47:00 -0700

    Added combobox for bids. Used jquery ui to make a combobox for bids. Small issue with hitting enter when you type doesn't select the element. If anyone has any ideas how to fix that let me know, I figured it was still better than the alternative.

--- static/play_game.js
+++ static/play_game.js
@@ -2,23 +2,151 @@ var cardImagePath = "static/card_images/";
 var allowedCards = [];
 var socket = io.connect();
 
+(function( $ ) {
+  $.widget( "custom.combobox", {
+    _create: function() {
+      this.wrapper = $( "<span>" )
+        .addClass( "custom-combobox" )
+        .insertAfter( this.element );
+
+      this.element.hide();
+      this._createAutocomplete();
+      this._createShowAllButton();
+    },
+
+    _createAutocomplete: function() {
+      var selected = this.element.children( ":selected" ),
+        value = selected.val() ? selected.text() : "";
+
+      this.input = $( "<input>" )
+        .appendTo( this.wrapper )
+        .val( value )
+        .attr( "title", "" )
+        .addClass( "custom-combobox-input ui-widget ui-widget-content ui-state-default ui-corner-left" )
+        .autocomplete({
+          delay: 0,
+          minLength: 0,
+          source: $.proxy( this, "_source" )
+        })
+        .tooltip({
+          tooltipClass: "ui-state-highlight"
+        });
+
+      this._on( this.input, {
+        autocompleteselect: function( event, ui ) {
+          ui.item.option.selected = true;
+          this._trigger( "select", event, {
+            item: ui.item.option
+          });
+        },
+
+        autocompletechange: "_removeIfInvalid"
+      });
+    },
+
+    _createShowAllButton: function() {
+      var input = this.input,
+        wasOpen = false;
+
+      $( "<a>" )
+        .attr( "tabIndex", -1 )
+        .attr( "title", "Show All Items" )
+        .tooltip()
+        .appendTo( this.wrapper )
+        .button({
+          icons: {
+            primary: "ui-icon-triangle-1-s"
+          },
+          text: false
+        })
+        .removeClass( "ui-corner-all" )
+        .addClass( "custom-combobox-toggle ui-corner-right" )
+        .mousedown(function() {
+          wasOpen = input.autocomplete( "widget" ).is( ":visible" );
+        })
+        .click(function() {
+          input.focus();
+
+          // Close if already visible
+          if ( wasOpen ) {
+            return;
+          }
+
+          // Pass empty string as value to search for, displaying all results
+          input.autocomplete( "search", "" );
+        });
+    },
+
+    _source: function( request, response ) {
+      var matcher = new RegExp( $.ui.autocomplete.escapeRegex(request.term), "i" );
+      response( this.element.children( "option" ).map(function() {
+        var text = $( this ).text();
+        if ( this.value && ( !request.term || matcher.test(text) ) )
+          return {
+            label: text,
+            value: text,
+            option: this
+          };
+      }) );
+    },
+
+    _removeIfInvalid: function( event, ui ) {
+
+      // Selected an item, nothing to do
+      if ( ui.item ) {
+        return;
+      }
+
+      // Search for a match (case-insensitive)
+      var value = this.input.val(),
+        valueLowerCase = value.toLowerCase(),
+        valid = false;
+      this.element.children( "option" ).each(function() {
+        if ( $( this ).text().toLowerCase() === valueLowerCase ) {
+          this.selected = valid = true;
+          return false;
+        }
+      });
+
+      // Found a match, nothing to do
+      if ( valid ) {
+        return;
+      }
+
+      // Remove invalid value
+      this.input
+        .val( "" )
+        .attr( "title", value + " didn't match any item" )
+        .tooltip( "open" );
+      this.element.val( "" );
+      this._delay(function() {
+        this.input.tooltip( "close" ).attr( "title", "" );
+      }, 2500 );
+      this.input.data( "ui-autocomplete" ).term = "";
+    },
+
+    _destroy: function() {
+      this.wrapper.remove();
+      this.element.show();
+    }
+  });
+})( jQuery );
+
 // Given a list of valid bids, will update the drop down list
 // of bids that a player can make
 function getBid(validBids){
-	var theBids = document.getElementById("bids");
-	
-	// first remove all previous valid bids
-	while (theBids.hasChildNodes()){
-		theBids.removeChild(theBids.lastChild);
-	}
-	
+	var bids = $("#bids");
+	bids.empty();
+
 	// update combo box of bids with valid bids
 	for (i = 0; i < validBids.length; i++){
 		aBid = document.createElement('option');
 		aBid.value = validBids[i];
 		aBid.innerHTML = validBids[i];
-		theBids.appendChild(aBid);
+		bids.append(aBid);
 	}
+  $(bids).combobox();
+  $('.custom-combobox-input').val('');
 }
 
 function returnBid() {
